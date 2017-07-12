31864cdc2fc82c86553bf74f5d86726314495f6f
Yeeun Jena Lee <yena6032@hotmail.com>
2017-07-12 15:14:13 +0900

    0712 skdigits_PCA, t-SNE, DBSCAN

--- sk_digits.py
+++ sk_digits.py
@@ -1,4 +1,6 @@
+import sys
 import matplotlib.pyplot as plt
+from time import time
 from sklearn.datasets import load_digits
 digits = load_digits()
 
@@ -6,13 +8,11 @@ fig, axes = plt.subplots(2, 5, figsize=(10,5), subplot_kw={'xticks':(), 'yticks'
 for ax, img in zip(axes.ravel(), digits.images):
     ax.imshow(img)
 
-
+t0=time()
 from sklearn.decomposition import PCA
 pca = PCA(n_components=2)
 pca = pca.fit(digits.data)
-#print digits.data
 digits_pca = pca.transform(digits.data)
-#print digits_pca
 colors = ["#476A2A","#7851B8",'#BD3430','#4A2D4E','#875525',
           '#A83683','#4E655E','#853541','#3A3120','#535D8E']
 
@@ -26,9 +26,10 @@ for i in range(len(digits.data)):
              fontdict={'weight':'bold', 'size':9})
 plt.xlabel("first PC")
 plt.ylabel("second PC")
-#plt.show()
+plt.show()
+print "PCA time: ", time()-t0
 
-
+t1 = time()
 from sklearn.manifold import TSNE
 tsne = TSNE(random_state=42)
 digits_tsne = tsne.fit_transform(digits.data)
@@ -44,8 +45,23 @@ for i in range(len(digits.data)):
 plt.xlabel("t-SNE feature 0")
 plt.ylabel("t-SNE feature 1")
 plt.show()
+print "t-SNE time: ", time()-t1
 
+#tSNE -> DBSCAN
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaler = scaler.fit(digits_tsne)
+scaled_t = scaler.transform(digits_tsne)
 
-from sklearn.cluster import KMeans
-kmeans = KMeans(n_clusters = 10)
-kmeans.fit(digits_tsne)
+t2 = time()
+from sklearn.cluster import DBSCAN
+dbscan = DBSCAN()
+clusters = dbscan.fit_predict(scaled_t)
+
+plt.scatter(scaled_t[:,0], scaled_t[:,1], c=clusters, s=60, edgecolors='black')
+plt.xlabel("feature 1")
+plt.ylabel("feature 2")
+plt.show()
+print "DBSCAN time: ", time()-t2
+
+#tSNE -> k-MEANS
